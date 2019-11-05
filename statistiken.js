a724d18c206ff631440e4fc630dae75bd12d96d8
Jorge Guerra <jorgeguerra1098@gmail.com>
2019-11-05 16:53:12 -0600

    ADD
    ADD: Building new operations.

--- statistiken.js
+++ statistiken.js
@@ -1,5 +1,19 @@
 const asc = require('./src/tools/order/asc');
 
-module.exports.orderAsc = array => {
+module.exports.orderAsc = function (array) {
     return asc(array);
 }
+
+module.exports.mean = function () {
+
+}
+
+module.exports.median = function () {
+
+}
+
+module.exports.mode = function () {
+
+}
+
+
