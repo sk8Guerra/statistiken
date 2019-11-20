029147aca9dbe18c2b98cf29bc307b155b876943
Jorge Guerra <jorgeguerra1098@gmail.com>
2019-11-20 14:10:27 -0600

    Update statistiken.js
    Trying a new structure.

--- statistiken.js
+++ statistiken.js
@@ -1,24 +1,24 @@
-const asc = require('./src/tools/order/asc');
 const mean = require('./src/arithmetic/mean');
 const median = require('./src/arithmetic/median');
 const mode = require('./src/arithmetic/mode');
 
-module.exports.orderAsc = function (array) {
-    return asc(array);
-}
+var _orderAsc = require('./src/tools/order/asc');
 
-module.exports.mean = function (array) {
+var _mean = function (array) {
     return mean(array, 0);
 }
 
-module.exports.median = function (array) {
+var _median = function (array) {
     return median(array);
 }
 
-module.exports.mode = function (array) {
+var _mode = function (array) {
     return mode(array);
 }
 
-
-
-
+module.exports {
+    orderAsc: _orderAsc,
+    mean: _mean,
+    median: _median,
+    mode: _mode
+}
