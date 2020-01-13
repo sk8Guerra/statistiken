7c699cdde0c73f685eeb7ebeafe58dfb70662e14
Coco Guerra <jorgeguerra1098@gmail.com>
2020-01-13 11:13:25 -0600

    Update postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,3 @@
-console.log(
-    "What do you think about this package?\n" +
-    "have any idea how to improve it?\n" +
-    "Rise an issue, so it can be better for everyone :) <3"
-);
+console.log("\x1b[36m%s\x1b[0m", "What do you think about this package?");
+console.log("\x1b[36m%s\x1b[0m", "have any idea how to improve it?");
+console.log("\x1b[33m%s\x1b[0m", "Rise an issue, so it can be better for everyone :) <3\n");
